097cfc5fb7c57e884becad0d49038e89e2f66954
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-05-14 13:57:11 -0400

    added selector criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,55 @@
-// Set Characters as variables
 var char = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   num: "1234567890",
-  specialChar: "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  specialChar: "!#$%&'()*+,-./\:;<=>?@[]^_`{|}~"
 }
-//User chooses characters
+//Choose characters
+let criteria = function() {
 
+  let choseChar = "";
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  while(choseChar === ""){
+    // select lowercase
+    let confirmLower = confirm('Include lowercase letters in your password?');
+    if(confirmLower === true){
+      alert("Your password will contain lowercase letters");
+      choseChar = choseChar + char.lowerCase;
+    }else{
+      alert("Your password will NOT contain lowercase letters")
+    }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // select uppercase
+    let confirmUpper = confirm('Include uppercase letters in your password?');
+    if(confirmUpper === true){
+      alert("Your password will contain uppercase letters");
+      choseChar = choseChar + char.upperCase;
+    }else{
+      alert("Your password will NOT contain uppercase letters")
+    }
 
-  passwordText.value = password;
+    // select numbers
+    let confirmNum = confirm('Include numbers in your password?');
+    if(confirmNum === true){
+      alert("Your password will contain numbers");
+      choseChar = choseChar + char.num;
+    }else{
+      alert("Your password will NOT contain numbers")
+    }
 
-}
+    // select special characters
+    let confirmSpec = confirm('Include special characters in your password?');
+    if(confirmSpec === true){
+      alert("Your password will contain special characters");
+      choseChar = choseChar + char.specialChar;
+    }else{
+      alert("Your password will NOT contain special characters");
+    }
+    // Alert if no choice
+    if(choseChar === ""){
+      alert("You must select at least one type of character");
+    }else{
+      return choseChar;
+    }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+   
